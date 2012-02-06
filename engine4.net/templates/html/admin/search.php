124c431132af0560262743f80c1d7b269c5ea058
Chris Lynch <github@planetofthepenguins.com>
2012-02-06 22:19:56 +0000

    New search template

---
+++ engine4.net/templates/html/admin/search.php
@@ -0,0 +1,14 @@
+<h2>Edit Existing Contents</h2>
+<table>
+	<tr><th>ID</th><th>Name</th><th>Last Modified</th><th>&nbsp</th></tr>
+	<?
+		foreach($data['page']['body']['content'] as $content){
+			print '<tr>';
+				print '<td>' . $content['ID'] . '</td>';
+				print '<td>' . $content['name'] . '</td>';
+				print '<td>' . $content['timestamp'] . '</td>';
+				print '<td><a href="?e4_action=admin&e4_op=edit&e4_ID=' . $content['ID'] . '">Edit</a></td>';
+			print '</tr>';
+		}
+	?>
+</table>
