61b1b298cca29d2d6fc9de28e2e4bd0998f9e51f
Chris Lynch <github@planetofthepenguins.com>
2012-02-12 23:47:31 +0000

    Product content type started...

---
+++ engine4.net/templates/html/admin/content-types/product.php
@@ -0,0 +1,4 @@
+<tr><td class="nobg" colspan="2"><h2 class="SectionHeading">Product Data</h2></td></tr>
+<tr><td>SKU</td><td><input name="e4_form_content_data_product_sku" type="text" size="30" value="<?=@$content['data']['product']['sku'];?>"></td></tr>			
+<tr><td>List Price/RRP</td><td><textarea name="e4_form_content_data_product_listprice" cols="40" rows="5"><?=@$content['data']['product']['listprice'];?></textarea></td></tr>
+<tr><td>Selling Price</td><td><textarea name="e4_form_content_data_product_sellingprice" cols="40" rows="5"><?=@$content['data']['product']['sellingprice'];?></textarea></td></tr>
