64b4dfc1aaf32feac323bd85a5ceb5aa3b8c1916
Chris Lynch <github@planetofthepenguins.com>
2012-02-27 17:36:05 +0000

    Last commit for today

--- engine4.net/templates/html/admin/forms/data-types/content.php
+++ engine4.net/templates/html/admin/forms/data-types/content.php
@@ -1,2 +1,2 @@
-<tr><td>Body</td><td><textarea name="e4_form_content_data_body" cols="70" rows="30"><?=@$content['data']['body'];?></textarea></td></tr>
-<tr><td>Teaser</td><td><textarea name="e4_form_content_data_teaser" cols="70" rows="10"><?=@$content['data']['teaser'];?></textarea></td></tr>
+<tr><td>Body</td><td><textarea name="e4_form_content_data_body" cols="60" rows="30"><?=@$content['data']['body'];?></textarea></td></tr>
+<tr><td>Teaser</td><td><textarea name="e4_form_content_data_teaser" cols="60" rows="10"><?=@$content['data']['teaser'];?></textarea></td></tr>
