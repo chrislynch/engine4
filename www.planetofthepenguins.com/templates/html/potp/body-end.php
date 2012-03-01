a50d7ffa44bf481d690986e1db7f1646ef0bddb8
Chris Lynch <github@planetofthepenguins.com>
2012-03-01 23:56:07 +0000

    Added POTP website. Will convert to skin.

---
+++ www.planetofthepenguins.com/templates/html/potp/body-end.php
@@ -0,0 +1,105 @@
+        </div>
+    </div>
+    <div class="span-1">&nbsp;</div>
+    <div class="span-5 last sidebar paper">
+        <div class="span-3 prepend-1 append-1">
+            <h3 class="sidebarheading">Tags</h3>
+        </div>
+    </div>
+</div>
+<div class="span-24 last footer paper">
+    <div class="span-7 prepend-1">
+        <h3>About Chris Lynch</h3>
+        <p>Chris Lynch is a writer and technologist.</p>
+        <p>Chris writes for <a href="http://www.markosia.com">Markosia</a>, one of the UK's leading comic book publishers.
+            His most recent graphic novel, <a href="http://www.thedarkcomicbook.com">The Dark</a>, is available now.</p>
+        <p>Chris is also one half of the creative team being the critically acclaimed horror anthology <a href="http://www.monkeyswithmachineguns.com">Monkeys with Machineguns</a></p>
+        <p>When not writing for MWM or Markosia, Chris has written for ...</p>
+        <p>During daylight hours, Chris is also a full time technologist. As Head of eCommerce Development for Gravit-e, ...
+    </div>
+    <div class="span-6 prepend-1 append-1">
+        <h3>About this website</h3>
+        <p>This website is powered by engine4.</p>
+        <p>engine4 is designed as a CMS that is easy for end-users to manage whilst being quick to implement and infinitely flexible for developers.</p>
+        <p>engine4 is an Open Source CMS project. You can access the source code and documentation for this project at GitHub.</p>
+    </div>
+    <div class="span-7 append-1 last">
+        <h3>Contact</h3>
+        
+    </div>
+</div>
+
+<style type="text/css">
+    .main{
+        margin-top: 100px;
+    }
+    .footer{
+        margin-top: 50px;
+    }
+    
+    .logo {
+        padding-top: 15px;
+        padding-bottom: 5px;
+        
+        margin-top: 5px;
+        margin-bottom: 10px;
+        
+        border-top: #000;
+        border-top-style: solid;
+        
+        border-bottom: #000;
+        border-bottom-style: double;
+                
+        font-family: serif;
+        font-weight: bolder;
+        font-size: xx-large;
+        
+        text-align: center;
+    }
+    .strapline{
+        background-color: burlywood;
+        font-size: medium;
+        padding-left: 160px;
+        padding-right: 160px;
+    }
+    .social{
+        font-size: small;
+        font-style: normal;
+        text-align: right;
+    }
+    .articleheading {
+        font-family: serif;
+        font-weight: bolder;
+        font-size: xx-large;
+    }
+    .articleteaserheading {
+        font-family: serif;
+        font-weight: bolder;
+        font-size: large;
+    }
+    
+    .sidebar {
+        
+    }
+    .sidebarheading {
+        border-top: medium solid #000000;       
+        font-family: serif;
+    }
+    
+    .paper {
+        background-image: url('@@configuration.basedir@@<?php print e4_findtemplate('images/paperbg.jpg'); ?>');
+        border-radius: 10px;
+    }
+    
+    .highlight {
+        background: burlywood;
+        margin-bottom: 25px;
+    }
+    
+    body {
+        background: #000;
+        background-image: url('@@configuration.basedir@@<?php print e4_findtemplate('images/bg.jpg'); ?>');
+        background-position: top center;
+        background-repeat: no-repeat;
+    }
+</style>
---
+++ www.planetofthepenguins.com/templates/html/potp/body-start.php
@@ -0,0 +1,8 @@
+<div class="span-24 last main">
+    <div class="span-18 paper">
+        <div class="span-16 prepend-1 append-1 logo">
+            PLANET OF THE PENGUINS<br>
+            <span class="strapline">PULP HORROR AND SCIENCE FICTION</span>
+        </div>
+        <div class="span-16 prepend-1 append-1 last">
+    
