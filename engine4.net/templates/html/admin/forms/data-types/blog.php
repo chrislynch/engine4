a20ef6b002a10a2eea0828d6a09c6b1cf1080058
Chris Lynch <github@planetofthepenguins.com>
2012-03-05 23:15:25 +0000

    Better definition on some types of data

--- engine4.net/templates/html/admin/forms/data-types/blog.php
+++ engine4.net/templates/html/admin/forms/data-types/blog.php
@@ -1,3 +1,3 @@
-<tr><td>Body</td><td><textarea name="e4_form_content_data_body" cols="60" rows="30"><?=@$content['data']['body'];?></textarea></td></tr>
-<tr><td>Teaser</td><td><textarea name="e4_form_content_data_teaser" cols="60" rows="10"><?=@$content['data']['teaser'];?></textarea></td></tr>
+<tr><td class="nobg" colspan="2"><h2 class="SectionHeading">Blog Tags</h2></td></tr>
+<tr><td>Blog Tags</td><td><textarea name="e4_form_content_data_body" cols="60" rows="3"><?=@$content['data']['body'];?></textarea></td></tr>
 
--- engine4.net/templates/html/admin/forms/data-types/owner.php
+++ engine4.net/templates/html/admin/forms/data-types/owner.php
@@ -3,5 +3,4 @@
     * To change this template, choose Tools | Templates
     * and open the template in the editor.
     */
-    
 ?>
