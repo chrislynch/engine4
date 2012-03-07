26d2f2cb688c6aa98bab02dad73d9009dc45fea4
Chris Lynch <github@planetofthepenguins.com>
2012-03-07 23:34:16 +0000

    Fiddling with layouts

--- engine4.net/templates/html/default/data-types/video.php
+++ engine4.net/templates/html/default/data-types/video.php
@@ -1,12 +1,15 @@
 <?php
     if (sizeof($data['page']['body']['content']) == 1){
         print '<h1 class="articleheading">' . @$content['name'] . '</h1>';
+        include e4_findtemplate('widgets/content-info.php');
         print '<iframe width="560" height="315" src="http://www.youtube.com/embed/' . @$content['data']['video']['youtubeid'] . '" frameborder="0" allowfullscreen></iframe>';
 	print Markdown(@$content['data']['body']); 
     } else {
         print '<h2 class="articleteaserheading"><a href="' . @$content['link'] . '">' . @$content['name'] . '</a></h2>';
         print '<img src="http://img.youtube.com/vi/' . @$content['data']['video']['youtubeid'] . '/1.jpg" style="float:left; padding-right: 10px">';
 	print Markdown(@$content['data']['teaser']); 
+        include e4_findtemplate('widgets/content-info.php');
         print '<div class="clear"></div>';
+        
     }
 ?>
