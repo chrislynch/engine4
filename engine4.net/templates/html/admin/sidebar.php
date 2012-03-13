1dac3ec92136bf86e3790f8974a7266fa923619d
Chris Lynch <github@planetofthepenguins.com>
2012-03-13 16:02:41 +0000

    Minor tidy ups to CMS look and feel

--- engine4.net/templates/html/admin/sidebar.php
+++ engine4.net/templates/html/admin/sidebar.php
@@ -1,5 +1,7 @@
 <div class="span-8 last">
 	<h2 class="SectionHeading">Help</h2>
-        
+        <?php
+            include e4_findtemplate('sidebar-content/' . $_REQUEST['e4_action'] . '-' . $_REQUEST['e4_op'] . '.php');
+        ?>
 </div>
 </div>
