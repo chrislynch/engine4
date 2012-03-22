3f7473060a4ecce763475411f20b448b86e51c6e
Chris Lynch <github@planetofthepenguins.com>
2012-03-22 21:57:47 +0000

    Added links to social icons

--- engine4.net/templates/html/default/body-start.php
+++ engine4.net/templates/html/default/body-start.php
@@ -2,6 +2,9 @@
     <div class="container">
         <div id="header" class="span-24 last">
             <div id="sitename" class="span-18"><a href="@@configuration.basedir@@"><?=@$data['page']['body']['H1']?></a></div>
-            <div id="sociallinks" class="span-6 last">Facebook|Twitter</div>
+            <div id="sociallinks" class="span-6 last">
+                <a href="http://www.facebook.com/<?=$data['configuration']['3rdparty']['facebook']['account']?>">Facebook</a>|
+                <a href="http://www.twitter.com/#!/<?=$data['configuration']['3rdparty']['twitter']['account']?>">Twitter</a>
+            </div>
         </div>
         <div id="content" class="span-18">
