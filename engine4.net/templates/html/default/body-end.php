20b4aa6b9f2396aee8dd517cb439122a1332f89a
Chris Lynch <github@planetofthepenguins.com>
2012-03-22 23:07:09 +0000

    Theme work. Default theme almost complete

--- engine4.net/templates/html/default/body-end.php
+++ engine4.net/templates/html/default/body-end.php
@@ -1,10 +1,11 @@
         <div id="pager" class="span-18 last">
             <?php include e4_findtemplate('widgets/pager.php'); ?>
         </div>
-        <div id="sidebar" class="span-6 last">
-            
-        </div>
-        <div id="footer" class="span-24 last">
-            About engine4.net
-        </div>
+    </div>
+    <div id="sidebar" class="span-5 prepend-1 last">
+        <?php include e4_findtemplate('widgets/tagcloud.php'); ?>
+    </div>
+    <div id="footer" class="span-24 last">
+        About engine4.net
+    </div>
 </div>
