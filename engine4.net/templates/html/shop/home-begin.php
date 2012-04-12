4c072e55b8936c26cc9d1e7d2e2207fda9966942
Chris Lynch <github@planetofthepenguins.com>
2012-04-12 22:58:21 +0100

    Homepage grids come to life

--- engine4.net/templates/html/shop/home-begin.php
+++ engine4.net/templates/html/shop/home-begin.php
@@ -1,3 +1,42 @@
-<div id="content-products-lastest" class="span-24 last">
-    
+<div id="content-products-latest" class="span-24 last">
+    <div class="span-24 last"><h2>New Products</h2></div>
+    <?php
+        foreach($data['page']['body']['contentByType'] as $type=>$contentarray){
+            template_shop_home_begin_grid($contentarray);
+        }
+        
+    ?>
 </div>
+
+<div id="content-products-bestsellers" class="span-24 last">
+    <div class="span-24 last"><h2>Best Sellers</h2></div>
+    <?php
+        foreach($data['page']['body']['contentByType'] as $type=>$contentarray){
+            template_shop_home_begin_grid($contentarray);
+        }
+        
+    ?>
+</div>
+
+<?php
+//TODO: This should be a general gridding function ?
+function template_shop_home_begin_grid($contentarray){
+    $grid = 0;
+    $items = 0;
+    $last = '';
+    $cols = 6;
+    $rows = 1;
+    
+    $maxitems = $cols * $rows;
+    foreach($contentarray as $ID=>$content){
+        $grid ++;
+        $items ++;
+        if ($grid % $cols == 0) { $last = ' last '; } else { $last = ''; }
+        include e4_findtemplate('data-types/product-home.php');
+        if ($items == $maxitems){
+            break;
+        }
+    }
+}
+
+?>
