eddf184afad380fdf90f19c3d3a00083844828ef
Chris Lynch <github@planetofthepenguins.com>
2012-04-22 23:20:31 +0100

    Getting on to primary product layout. Need to work on importer now, to build better test data.

--- engine4.net/templates/html/shop/data-types/product-view-body.php
+++ engine4.net/templates/html/shop/data-types/product-view-body.php
@@ -1,16 +1,16 @@
-<div class="span-24 last product-view">
+<div class="span-15 append-1 last product-view">
     <div class="span-8 product-image">
         <a href="<?=@$content['link'];?>">
             <img src="<?php print $content['data']['files']['images']['primary']['path']; ?>">
         </a>
     </div>
-    <div class="span-10 product-data">
-        <div class="span-10 product-price">
+    <div class="span-7 last product-data">
+        <div class="span-7 last product-price">
             <span class="wasprice">Was: &pound;<?=@$content['data']['product']['listprice'];?></span><br>
             <span class="sellingprice">Now: &pound;<?=@$content['data']['product']['sellingprice'];?></span><br>
             <span class="saving"><?=@$content['data']['product']['savingtext'];?></span>
         </div>
-        <div class="product-addtocart">
+        <div class="span-7 last product-addtocart">
             <form action="@@configuration.basedir@@" method="POST">
                 <input type="hidden" name="e4_action" value="cart">
                 <input type="hidden" name="e4_cart_op" value="add">
@@ -19,4 +19,15 @@
             </form>
         </div>
     </div>
+    <div class="span-15 append-1 last">
+        <h2>Description</h2>
+        <?=$content['data']['body'];?>
+        <h2>Specifications</h2>
+        <?=$content['data']['body'];?>
+        <h2>Reviews</h2>
+        This product has no reviews.
+    </div>
+</div>
+<div class="span-8 last">
+    <h3>Other Bestsellers</h3>
 </div>
