e5d57c21fe01950286c14b6aa795e832303cefb0
Chris Lynch <github@planetofthepenguins.com>
2012-04-22 21:40:30 +0100

    Started working on nice layout for search

---
+++ engine4.net/templates/html/shop/search-begin.php
@@ -0,0 +1,8 @@
+<div id="banner-top" class="span-24 last">
+    <div class="banner-item span-24 last">
+        <h1>Search Results</h1>
+    </div>
+</div>
+<div id="banner-bottom" class="span-24 last">
+
+</div>
