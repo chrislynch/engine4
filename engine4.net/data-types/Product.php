eff8c1ed1b184762e2180c398e40eb7f78a1c0b2
Chris Lynch <github@planetofthepenguins.com>
2012-04-23 22:38:26 +0100

    Fixed product load

--- engine4.net/data-types/Product.php
+++ engine4.net/data-types/Product.php
@@ -3,6 +3,10 @@
 function e4_datatype_Product_load(&$content){
     // Perform "load" operations.
     
+    if (!isset($content['data']['product'])){ $content['data']['product'] = array();}
+    if (!isset($content['data']['product']['listprice'])){ $content['data']['product']['listprice'] = 0.00;}
+    if (!isset($content['data']['product']['sellingprice'])){ $content['data']['product']['sellingprice'] = 0.00;}
+    
     // Calculate our saving data.
     if ($content['data']['product']['listprice'] > $content['data']['product']['sellingprice']) {
         $content['data']['product']['saving'] = $content['data']['product']['listprice'] - $content['data']['product']['sellingprice'];
