54d0897aef7fbf8fd86e709282ded996c87e7960
Chris Lynch <chris@chris-Aspire-8943G.(none)>
2013-01-03 00:07:50 +0000

    New rapid prototyping engine .. in prototype

---
+++ _e/e.php
@@ -0,0 +1,62 @@
+<?php
+
+/*
+ * 
+ */
+
+class e {
+    
+    function go() {
+        
+        // Get initial list of directories
+        $directories = scandir('.');
+        
+        foreach($directories as $directory){
+            // Work through directories, processing each in order.
+            if ($this->isValidDirectory($directory)){
+                $this->open($directory);
+            }
+        }
+        
+        print 'Hello World';
+    }
+    
+    private function find($inDirectory){
+        if (isset($_REQUEST['q'])){
+            $q = $_REQUEST['q'];
+        } else {
+            $q = '';
+        }
+        
+        while(strlen($q) > 0){
+            print "Looking for $q in $inDirectory<br>";
+            if (file_exists($inDirectory . '/' . $q)){
+                if ($this->isValidDirectory($inDirectory . '/' . $q)){
+                    
+                }
+            } else {
+                $q = $this->dirup($q);
+            }
+        }
+    }
+    
+    private function dirup($directory){
+        $directory = explode('/',$directory);
+        array_pop($directory);
+        $directory = implode('/',$directory);
+        return $directory;
+    }
+    
+    private function isValidDirectory($directory){
+        $return = is_dir($directory);
+        if ($return){
+            if ($directory == '.') { $return = FALSE; }
+            if ($directory == '..') { $return = FALSE; }
+            if (substr($directory,0,1) == '.') { $return = FALSE; }
+            if (substr($directory,0,1) == '_') { $return = FALSE; }
+        }
+        return $return;
+    }
+}
+
+?>
--- void.php
+++
@@ -1,7 +0,0 @@
-<?php 
-/*
-	Hello. I am the void file.
- 	I am empty, and always should be.
- 	I am the file that is included if all other attempts to find a file to include fail
-*/
-?>
