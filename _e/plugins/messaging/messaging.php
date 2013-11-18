17838c13ee2d3039d8ab98b3d1a9ff36b7f6d8aa
Chris Lynch <github@planetofthepenguins.com>
2013-11-18 11:04:36 +0000

    Added support for PHP mail() as an alternative to SMTP

--- _e/plugins/messaging/messaging.php
+++ _e/plugins/messaging/messaging.php
@@ -37,54 +37,68 @@ class _messaging {
     }
     
     function sendMessage($toAddress,$toName,$subject,$message,$fromAddress = '', $fromName = '', $replyAddress = '', $replyName = '', $private = FALSE){
-        require_once('_e/lib/PHPMailer/class.phpmailer.php');
-        //include("class.smtp.php"); // optional, gets called from within class.phpmailer.php if not already loaded
-
-        $mail = new PHPMailer(true); // the true param means it will throw exceptions on errors, which we need to catch
-
-        $mail->IsSMTP(); // telling the class to use SMTP
-
-        try {
-            $mail->Host       = $this->e->_config->get('smtp.host'); // "mail.yourdomain.com"; // SMTP server
-            $mail->SMTPDebug  = 0;                              // 2; // enables SMTP debug information (for testing)
-            $mail->SMTPAuth   = true;                           // enable SMTP authentication
-            $mail->Port       = $this->e->_config->get('smtp.port'); //26; // set the SMTP port for the GMAIL server
-            $mail->Username   = $this->e->_config->get('smtp.user'); // "yourname@yourdomain"; // SMTP account username
-            $mail->Password   = $this->e->_config->get('smtp.password'); // "yourpassword";        // SMTP account password
-            
-            // To
-            $mail->AddAddress($toAddress, $toName);
-            
-            // From
-            if (strlen($fromAddress) == 0){ $fromAddress = $this->e->_config->get('smtp.from.Address'); }
-            if (strlen($fromName) == 0){ $fromName = $this->e->_config->get('smtp.from.Name'); }
-            if (strlen($replyAddress) == 0){ $replyAddress = $this->e->_config->get('smtp.reply.Address'); }
-            if (strlen($replyName) == 0){ $replyName = $this->e->_config->get('smtp.reply.Name'); }
-            
-            $mail->SetFrom($fromAddress, $fromName);
-        	if(strlen($replyAddress) > 0 AND strlen($replyName) > 0){
-            	$mail->AddReplyTo($replyAddress, $replyName);
-            }
-            
-            // Subject and Message
-            $mail->Subject = $subject;
-            $mail->MsgHTML($message);
-            // $mail->AltBody = 'To view the message, please use an HTML compatible email viewer!'; // optional - MsgHTML will create an alternate automatically
-            
-            // Attachments
-            /*
-            $mail->AddAttachment('images/phpmailer.gif');      // attachment
-            $mail->AddAttachment('images/phpmailer_mini.gif'); // attachment
-            */
-            
-            $mail->Send();
-            if (!$private){$this->addMessage("email sent to $toAddress");}
-            return TRUE;
-            
-        } catch (phpmailerException $e) {
-            if (!$private){$this->addMessage("Unable to send email to $toAddress. eMail system said " . $e->errorMessage(),'error');} 
-            return FALSE;
-        }
+    	// Defaults
+    	if (strlen($fromAddress) == 0){ $fromAddress = $this->e->_config->get('smtp.from.Address'); }
+    	if (strlen($fromName) == 0){ $fromName = $this->e->_config->get('smtp.from.Name'); }
+    	if (strlen($replyAddress) == 0){ $replyAddress = $this->e->_config->get('smtp.reply.Address'); }
+    	if (strlen($replyName) == 0){ $replyName = $this->e->_config->get('smtp.reply.Name'); }
+    	
+    	if (strlen($this->e->_config->get('smtp.host') > 0)){
+    		require_once('_e/lib/PHPMailer/class.phpmailer.php');
+    		//include("class.smtp.php"); // optional, gets called from within class.phpmailer.php if not already loaded
+    		
+    		$mail = new PHPMailer(true); // the true param means it will throw exceptions on errors, which we need to catch
+    		
+    		$mail->IsSMTP(); // telling the class to use SMTP
+    		
+    		try {
+    			$mail->Host       = $this->e->_config->get('smtp.host'); // "mail.yourdomain.com"; // SMTP server
+    			$mail->SMTPDebug  = 0;                              	 // 2; // enables SMTP debug information (for testing)
+    			$mail->SMTPAuth   = true;                           	 // enable SMTP authentication
+    			$mail->Port       = $this->e->_config->get('smtp.port'); //26; // set the SMTP port for the GMAIL server
+    			$mail->Username   = $this->e->_config->get('smtp.user'); // "yourname@yourdomain"; // SMTP account username
+    			$mail->Password   = $this->e->_config->get('smtp.password'); // "yourpassword";        // SMTP account password
+    		
+    			// To
+    			$mail->AddAddress($toAddress, $toName);
+    		
+    			// From
+    			$mail->SetFrom($fromAddress, $fromName);
+    			if(strlen($replyAddress) > 0 AND strlen($replyName) > 0){
+    				$mail->AddReplyTo($replyAddress, $replyName);
+    			}
+    		
+    			// Subject and Message
+    			$mail->Subject = $subject;
+    			$mail->MsgHTML($message);
+    			// $mail->AltBody = 'To view the message, please use an HTML compatible email viewer!'; // optional - MsgHTML will create an alternate automatically
+    		
+    			// Attachments
+    			/*
+    			$mail->AddAttachment('images/phpmailer.gif');      // attachment
+    			$mail->AddAttachment('images/phpmailer_mini.gif'); // attachment
+    			*/
+    		
+    			$mail->Send();
+    			if (!$private){$this->addMessage("email sent to $toAddress");}
+    			return TRUE;
+    		
+    		} catch (phpmailerException $e) {
+    				if (!$private){$this->addMessage("Unable to send email to $toAddress. eMail system said " . $e->errorMessage(),'error');}
+    				return FALSE;
+			}	
+    	} else {
+    		// SMTP not configured. Use PHP's mail() instead.
+    		$to      = $toAddress;
+    		$subject = $subject;
+    		$message = $message;
+    		$headers = 'From: ' . $fromAddress . "\r\n" .
+    				   'Reply-To: ' . $replyAddress . "\r\n" .
+    				   'X-Mailer: PHP/' . phpversion();
+    		
+    		return mail($to, $subject, $message, $headers);
+    	}
+        
     }
 }
 
