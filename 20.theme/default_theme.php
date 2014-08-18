2c5cdeb00b9b8c90d1d75f6f8fd005df63aaaa75
Chris Lynch <github@planetofthepenguins.com>
2014-08-18 13:15:12 +0100

    Overidding home changes

---
+++ 20.theme/default_theme.php
@@ -0,0 +1,33 @@
+<html>
+<head>
+	<base href="<?= $this->_basehref() ?>">
+		
+	<!-- JQuery -->
+	<script src="http://code.jquery.com/jquery-2.1.1.min.js"></script>
+		
+	<!-- Bootstrap -->
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.min.css">
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+	
+	<!-- Our stylesheet - overrides -->
+	<link href="style.css" rel=stylesheet type="text/css">
+	
+</head>
+<body>
+	<h1><?php print $this->content->title;?></h1>
+	<?php print $this->content->html; print $this->app->html; ?>	
+</body>
+</html>
+
+
+
+	
+	
+
+
+
+
