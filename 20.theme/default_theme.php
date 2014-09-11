7ecff3a972dbab635e473527d2927bb5bfa6b459
Chris Lynch <github@planetofthepenguins.com>
2014-09-11 13:35:56 +0100

    RSS feeds

--- 20.theme/default_theme.php
+++ 20.theme/default_theme.php
@@ -7,7 +7,12 @@ $loader = new Twig_Loader_Filesystem('_custom/_default/content/templates');
 // $twig = new Twig_Environment($loader, array('cache' => '_custom/_default/content/cache/templates',));
 $twig = new Twig_Environment($loader, array());
 
-$template = $twig->loadTemplate('index.html');
+if (isset($_GET['feed'])){
+	$template = $twig->loadTemplate('feed.html');
+} else {
+	$template = $twig->loadTemplate('index.html');
+}
+
 $templatevars = get_object_vars($this);
 $templatethis = array();
 foreach($templatevars as $var => $value){
